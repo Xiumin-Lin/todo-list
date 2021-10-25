2cb4becbc5e1d3891be57091bf01e79417fe1cc5
steven-van <64859587+steven-van@users.noreply.github.com>
2021-10-25 17:37:45 +0200

    fix: change title and format todo

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -5,7 +5,7 @@ const todos = (state = [], action) => {
         ...state,
         {
           id: action.id,
-          text: action.text + " | Published : " + action.date,
+          text: action.text + " | " + action.date,
           completed: false,
         },
       ];
