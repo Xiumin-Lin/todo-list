f49d89f17c617f88f4ead8f5442a26db21695f96
steven-van <64859587+steven-van@users.noreply.github.com>
2021-10-25 21:40:13 +0200

    fix: remove useless div and add return todolist

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import TodoBis from "./TodoBis";
 import RemoveTodo from "../containers/RemoveTodo";
+import ThemeContext from "../contexts/ThemeContext";
 
-const TodoList = ({ todos, toggleTodo }) => (
-  <ul>
-    {todos.map((todo) => (
-      <div className={"flex mb-4 items-center"}>
-        <TodoBis key={todo.id} {...todo} onClick={() => toggleTodo(todo.id)} />
-        <RemoveTodo id={todo.id} />
-      </div>
-    ))}
-  </ul>
-);
+const TodoList = ({ todos, toggleTodo }) => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <div
+          className={`flex rounded px-3 py-2 mb-4 items-center ${
+            theme === "dark" ? "bg-gray-800" : "bg-gray-300"
+          }`}
+        >
+          <TodoBis
+            key={todo.id}
+            {...todo}
+            onClick={() => toggleTodo(todo.id)}
+          />
+          <RemoveTodo id={todo.id} />
+        </div>
+      ))}
+    </ul>
+  );
+};
 
 TodoList.propTypes = {
   todos: PropTypes.arrayOf(
--- src/containers/RemoveTodo.js
+++ src/containers/RemoveTodo.js
@@ -4,31 +4,29 @@ import { deleteTodo } from "../actions";
 
 const RemoveTodo = ({ id, dispatch }) => {
   return (
-    <div>
-      <button
-        className={
-          "p-3 focus:outline-none rounded-full font-bold text-white bg-red-600"
-        }
-        onClick={(e) => {
-          e.preventDefault();
-          dispatch(deleteTodo(id));
+    <button
+      className={
+        "p-3 focus:outline-none rounded-full font-bold text-white bg-red-600"
+      }
+      onClick={(e) => {
+        e.preventDefault();
+        dispatch(deleteTodo(id));
 
-          console.log(dispatch(deleteTodo(id)));
-        }}
+        console.log(dispatch(deleteTodo(id)));
+      }}
+    >
+      <svg
+        className={"w-5 h-5"}
+        viewBox="0 0 15 17"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <svg
-          className={"w-5 h-5"}
-          viewBox="0 0 15 17"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M2.5 4.16667H1.66667V15C1.66667 15.442 1.84226 15.866 2.15482 16.1785C2.46738 16.4911 2.89131 16.6667 3.33333 16.6667H11.6667C12.1087 16.6667 12.5326 16.4911 12.8452 16.1785C13.1577 15.866 13.3333 15.442 13.3333 15V4.16667H2.5ZM11.3483 1.66667L10 0H5L3.65167 1.66667H0V3.33333H15V1.66667H11.3483Z"
-            fill="white"
-          />
-        </svg>
-      </button>
-    </div>
+        <path
+          d="M2.5 4.16667H1.66667V15C1.66667 15.442 1.84226 15.866 2.15482 16.1785C2.46738 16.4911 2.89131 16.6667 3.33333 16.6667H11.6667C12.1087 16.6667 12.5326 16.4911 12.8452 16.1785C13.1577 15.866 13.3333 15.442 13.3333 15V4.16667H2.5ZM11.3483 1.66667L10 0H5L3.65167 1.66667H0V3.33333H15V1.66667H11.3483Z"
+          fill="white"
+        />
+      </svg>
+    </button>
   );
 };
 
